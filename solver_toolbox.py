af37f2f95d75826d813dfe2e71c60b5c50cfbdef
ajain-23 <ayush.jain@berkeley.edu>
2019-11-27 12:40:37 -0800

    regular tsp solver

--- solver_toolbox.py
+++ solver_toolbox.py
@@ -3,6 +3,7 @@ import itertools
 from itertools import product
 from sys import stdout as out
 from mip import Model, xsum, minimize, BINARY
+import utils
 
 class BaseSolver:
     """ Base class for solvers """
@@ -147,36 +148,8 @@ class ILPSolver(BaseSolver):
         return best_solution
         '''
 
-        # names of places to visit
-        places = ['Dwinelle', 'Soda', 'Cory', 'Blum', 'Etcheverry',
-                'Campanile', 'Wheeler', 'Memorial Stadium',
-                'Li Ka Shing', 'Main Stacks', 'Jacobs', 'Wurster',
-                'Sutardja Dai', 'Barrows']
-
-        # distances in an upper triangular matrix
-        dists = [[83, 81, 113, 52, 42, 73, 44, 23, 91, 105, 90, 124, 57],
-                [161, 160, 39, 89, 151, 110, 90, 99, 177, 143, 193, 100],
-                [90, 125, 82, 13, 57, 71, 123, 38, 72, 59, 82],
-                [123, 77, 81, 71, 91, 72, 64, 24, 62, 63],
-                [51, 114, 72, 54, 69, 139, 105, 155, 62],
-                [70, 25, 22, 52, 90, 56, 105, 16],
-                [45, 61, 111, 36, 61, 57, 70],
-                [23, 71, 67, 48, 85, 29],
-                [74, 89, 69, 107, 36],
-                [117, 65, 125, 43],
-                [54, 22, 84],
-                [60, 44],
-                [97],
-                []]
-
         # number of nodes and list of vertices
-        n, V = len(dists), range(len(dists))
-
-        # distances matrix
-        c = [[0 if i == j
-            else dists[i][j-i-1] if j > i
-            else dists[j][i-j-1]
-            for j in V] for i in V]      
+        n, V, H = len(list_of_locations), range(len(list_of_locations)), range(len(list_of_homes))    
 
         model = Model()
 
@@ -188,7 +161,7 @@ class ILPSolver(BaseSolver):
         y = [model.add_var() for i in V]
 
         # objective function: minimize the distance
-        model.objective = minimize(xsum(c[i][j]*x[i][j] for i in V for j in V))
+        model.objective = minimize(xsum(adjacency_matrix[i][j]*x[i][j] for i in V for j in V))
 
         # constraint : leave each city only once
         for i in V:
@@ -203,19 +176,25 @@ class ILPSolver(BaseSolver):
                 model += y[i] - (n+1)*x[i][j] >= y[j]-n
 
         # optimizing
-        model.optimize(max_seconds = 30)
+        model.optimize()
 
         # checking if a solution was found
         if model.num_solutions:
-            out.write('Route with total distance %g found: %s' % (model.objective_value, places[0]))
+            out.write('Route with total distance %g found: %s' % (model.objective_value, starting_car_location))
             nc = 0
             while True:
                 nc = [i for i in V if x[nc][i].x >= 0.99][0]
-                out.write(' -> %s' % places[nc])
+                out.write(' -> %s' % list_of_locations[nc])
                 if nc == 0:
                     break
             out.write('\n')
 
 
 tsp = ILPSolver()
-tsp.solve(None, None, None, None)
+input_data = utils.read_file("inputs/300_50.in")
+num_of_locations, num_houses, list_locations, list_houses, starting_car_location, adjacency_matrix = data_parser(input_data)
+for i in range(len(adjacency_matrix)):
+    for j in range(len(adjacency_matrix)):
+        if (adjacency_matrix[i][j] == 'x'):
+            adjacency_matrix[i][j] = 0
+tsp.solve(list_locations, list_houses, starting_car_location, adjacency_matrix)
