319893e10b4b073b98cba0a43d727b698edf51e1
ajain-23 <ayush.jain@berkeley.edu>
2019-11-26 17:49:48 -0800

    basic tsp solver

--- solver_toolbox.py
+++ solver_toolbox.py
@@ -1,5 +1,8 @@
 from student_utils import *
 import itertools
+from itertools import product
+from sys import stdout as out
+from mip import Model, xsum, minimize, BINARY
 
 class BaseSolver:
     """ Base class for solvers """
@@ -131,6 +134,7 @@ class ILPSolver(BaseSolver):
             A dictionary mapping drop-off location to a list of homes of TAs that got off at that particular location
             NOTE: all outputs should be in terms of indices not the names of the locations themselves
         """
+        '''
         home_indices = convert_locations_to_indices(list_of_homes, list_of_locations)
 
         G, message = adjacency_matrix_to_graph(adjacency_matrix)
@@ -141,3 +145,77 @@ class ILPSolver(BaseSolver):
 
         print("\n\nBest cost was", best_solution[0])
         return best_solution
+        '''
+
+        # names of places to visit
+        places = ['Dwinelle', 'Soda', 'Cory', 'Blum', 'Etcheverry',
+                'Campanile', 'Wheeler', 'Memorial Stadium',
+                'Li Ka Shing', 'Main Stacks', 'Jacobs', 'Wurster',
+                'Sutardja Dai', 'Barrows']
+
+        # distances in an upper triangular matrix
+        dists = [[83, 81, 113, 52, 42, 73, 44, 23, 91, 105, 90, 124, 57],
+                [161, 160, 39, 89, 151, 110, 90, 99, 177, 143, 193, 100],
+                [90, 125, 82, 13, 57, 71, 123, 38, 72, 59, 82],
+                [123, 77, 81, 71, 91, 72, 64, 24, 62, 63],
+                [51, 114, 72, 54, 69, 139, 105, 155, 62],
+                [70, 25, 22, 52, 90, 56, 105, 16],
+                [45, 61, 111, 36, 61, 57, 70],
+                [23, 71, 67, 48, 85, 29],
+                [74, 89, 69, 107, 36],
+                [117, 65, 125, 43],
+                [54, 22, 84],
+                [60, 44],
+                [97],
+                []]
+
+        # number of nodes and list of vertices
+        n, V = len(dists), range(len(dists))
+
+        # distances matrix
+        c = [[0 if i == j
+            else dists[i][j-i-1] if j > i
+            else dists[j][i-j-1]
+            for j in V] for i in V]      
+
+        model = Model()
+
+        # binary variables indicating if arc (i,j) is used on the route or not
+        x = [[model.add_var(var_type=BINARY) for j in V] for i in V]
+
+        # continuous variable to prevent subtours: each city will have a
+        # different sequential id in the planned route except the first one
+        y = [model.add_var() for i in V]
+
+        # objective function: minimize the distance
+        model.objective = minimize(xsum(c[i][j]*x[i][j] for i in V for j in V))
+
+        # constraint : leave each city only once
+        for i in V:
+            model += xsum(x[i][j] for j in set(V) - {i}) == 1
+
+        # constraint : enter each city only once
+        for i in V:
+            model += xsum(x[j][i] for j in set(V) - {i}) == 1
+
+        # subtour elimination
+        for (i, j) in set(product(set(V) - {0}, set(V) - {0})):
+                model += y[i] - (n+1)*x[i][j] >= y[j]-n
+
+        # optimizing
+        model.optimize(max_seconds = 30)
+
+        # checking if a solution was found
+        if model.num_solutions:
+            out.write('Route with total distance %g found: %s' % (model.objective_value, places[0]))
+            nc = 0
+            while True:
+                nc = [i for i in V if x[nc][i].x >= 0.99][0]
+                out.write(' -> %s' % places[nc])
+                if nc == 0:
+                    break
+            out.write('\n')
+
+
+tsp = ILPSolver()
+tsp.solve(None, None, None, None)
