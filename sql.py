c42cf96f1759130f5fd4bd74fd0782e874ea29b7
Jaiveer Singh <j.singh@berkeley.edu>
2019-12-03 01:53:47 -0800

    sql.py has modes: print, query, merge

--- sql.py
+++ sql.py
@@ -33,21 +33,81 @@ def run_queries():
         print()
 
 def merge_tables():
+
+    NUM_ENTRIES = 500
+
     saved_tables = utils.get_files_with_extension("models/", 'sqlite')
 
     local_conn = sqlite3.connect('models.sqlite')
     local_cursor = local_conn.cursor()
+    orig_local_res = local_cursor.execute("SELECT * FROM models ORDER BY input_file LIMIT {}".format(NUM_ENTRIES)).fetchall()
+    
     for table in saved_tables:
         print(table)
         remote_conn = sqlite3.connect(table)
         remote_cursor = remote_conn.cursor()
 
-        local_results = local_cursor.execute("SELECT * FROM models ORDER BY input_file LIMIT 5").fetchall()
-        remote_results = remote_cursor.execute("SELECT * FROM models ORDER BY input_file LIMIT 5 ").fetchall()
+        local_results = local_cursor.execute("SELECT * FROM models ORDER BY input_file LIMIT {}".format(NUM_ENTRIES)).fetchall()
+        remote_results = remote_cursor.execute("SELECT * FROM models ORDER BY input_file LIMIT {}".format(NUM_ENTRIES)).fetchall()
 
-        combined_results = []
-        #print(local_results)
-        print(remote_results)
+        best_results = []
+
+        def compare(a, b):
+            if a[0] > b[0]:
+                return a, True, False
+            if b[0] > a[0]:
+                return b, False, True
+
+            if a[2] and not b[2]:
+                return a, True, True
+            if not a[2] and b[2]:
+                return b, True, True
+
+            if not a[2] and not b[2]:
+                if a[1] < b[1]:
+                    return a, True, True
+                return b, True, True
+            
+            if a[2] and b[2]:
+                if a[1] != b[1]:
+                    print("DANGER: entries claim to both be optimal with different costs")
+                    print(a)
+                    print(b)
+                return a, True, True
+
+
+        while len(local_results) > 0 and len(remote_results) > 0:
+            res_l = local_results[0]
+            res_r = remote_results[0]
+
+            better_result, pop_local, pop_remote = compare(res_l, res_r)
+
+            best_results.append(better_result)
+            if pop_local:
+                local_results.pop(0)
+            if pop_remote:
+                remote_results.pop(0)
+
+        best_results += local_results + remote_results            
+        
+        remote_conn.close()
+    local_conn.close()
+
+    new_table = input("Where to save new output? ")
+    new_conn = sqlite3.connect(new_table)
+    new_cursor = new_conn.cursor()
+    new_cursor.execute("CREATE TABLE IF NOT EXISTS models (input_file TEXT PRIMARY KEY, best_objective_bound NUMERIC, optimal INTEGER)")
+    for result in best_results:
+        new_cursor.execute('REPLACE INTO models (input_file, best_objective_bound, optimal) VALUES (?, ?, ?)', result)
+    new_conn.close()
+    
+    print("Local: ")
+    [print(i) for i in orig_local_res]
+    print()
+
+    print("Best: ")
+    [print(i) for i in best_results]
+    print()
 
 
 if __name__=="__main__":
